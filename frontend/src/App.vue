742bfb4ab4258194bd9dc9e36235b8a1fa0785ed
Célian Niclot <celian.niclot@gmail.com>
2021-04-23 10:53:33 +0200

    :sparkles: create userStore & interfaces

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,12 +8,14 @@
 import { defineComponent, SetupContext, onMounted } from 'vue';
 import { isUserLoggedIn } from './mixins/auth/auth.mixins';
 import { useRouter } from 'vue-router';
+import { userStoreProvider } from './store/user/user.store';
 
 export default defineComponent({
   name: 'App',
   setup(props, context: SetupContext) {
+    userStoreProvider();
     const router = useRouter();
-    
+
     onMounted(function () {
       const isLogged = isUserLoggedIn();
       if (!isLogged) return router.push('/login');
---
+++ frontend/src/store/user/user.store.ts
@@ -0,0 +1,39 @@
+import { ICreateUser } from '@/interface/user/user';
+import { inject, provide } from 'vue';
+import { userStore } from './state';
+import { useApi } from '../../mixins/api/api.mixins';
+import Cookies from 'js-cookie';
+import { useRouter } from 'vue-router';
+
+export const userStoreProvider = () => {
+  provide('userStore', userStore);
+};
+
+export function useUser() {
+  const { setUser, updateUser,clearUser, ...rest } = inject('userStore') as typeof userStore;
+
+  const { loginCall, signupCall } = useApi();
+  const router = useRouter();
+
+  const login = async (email: string, password: string) => {
+    const user = await loginCall(email, password);
+    if (!user.token) return;
+
+    router.push({ name: 'Home' });
+    Cookies.set('groupomania_token', user.token, { expires: 7 });
+  };
+
+  const signup = async (user: ICreateUser) => {
+    const userCreated = await signupCall(user);
+    if (!userCreated.email || !userCreated.active) return console.log('userCreated :',userCreated);
+
+    await login(userCreated.email, user.password);
+  };
+  const logout = () => {
+    Cookies.remove('groupomania_token');
+    clearUser()
+    router.push({ name: 'Login' });
+  };
+
+  return { setUser, updateUser, login, signup, logout, ...rest };
+}
