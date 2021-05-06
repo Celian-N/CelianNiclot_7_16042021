43f6a7d37f3bc1e3c914a1c9dd39c9bf99c20756
Célian Niclot <celian.niclot@gmail.com>
2021-05-06 14:46:37 +0200

    create Publications page, Publication & publicationInput component

---
+++ frontend/src/components/Publications/Publications.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <PublicationInput />
+    <div class="column items-center">
+      <div v-for="publication in publications" :key="`publication-${publication.id}`">
+        <Publication
+          :publication="publication"
+          :user="user"
+          @onDeletePublication="(val) => $emit('deletePublication', val)"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { IPublication } from '../../interface/publications/publication';
+import Publication from './Publication.vue';
+import PublicationInput from './PublicationInput.vue';
+import { IUser } from '../../interface/user/user';
+
+export default defineComponent({
+  name: 'Publications',
+  props: {
+    publications: { type: Array as PropType<IPublication[]> },
+    user: { type: Object as PropType<IUser>, required: true },
+  },
+  components: {
+    Publication,
+    PublicationInput,
+  },
+});
+</script>
