8d099b94c5b2f138b8f42bc6f505bdd8f839552a
Célian Niclot <celian.niclot@gmail.com>
2021-05-21 09:34:44 +0200

    create Notifications & Settings page to test navigation

---
+++ frontend/src/pages/Notifications.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    NOTIFICATIONS
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name:'Notifications'
+  
+})
+</script>
